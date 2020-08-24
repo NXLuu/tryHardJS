48e1d13b9c32c8e8fa5c986570d69a25926e47b5
Luu Nguyen <nxuluu@gmail.com>
2020-08-24 14:51:31 +0700

    Learn some basic command of git

--- Learn_Git.js
+++ Learn_Git.js
@@ -1,1 +1,8 @@
 // Chúng ta
+// Git commit -m "": Dùng để lưu một trạng thái thay đổi 
+// git checkout -b [yourbranchname]: Dùng để di chuyển đến branch
+// git checkout [Nhánh]: Để đi nhánh đã được tạo
+// git merge [Nhánh]: Dùng để hợp nhất â trạng thái
+// git rebase [Nhánh]: Tạo một trạng thái mới từ trạng thái của nhánh
+// git checkout [Trạng thái]: Dùng để đưa một trạng thái vào HEAD - Trạng thái làm việc
+// git checkout [Nhánh/HEAD]: dùng để đưa nhánh hoặc HEAD về trạng thái trước
