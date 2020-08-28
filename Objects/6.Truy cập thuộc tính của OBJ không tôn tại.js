578cb693240ece5133117e582328b0eb4e07eddb
Luu Nguyen <nxuluu@gmail.com>
2020-08-28 16:43:45 +0700

    Optional chain ?.

---
+++ Objects/6.Truy cập thuộc tính của OBJ không tôn tại.js
@@ -0,0 +1,7 @@
+﻿/* 
+    The optional chaining ?. sẽ trả về undefinded  nếu phần đứng  trước ?. là undefied hoặc null 
+
+*/
+let user = {}; // user has no address
+
+alert( user?.address?.street ); // undefined (no error)
