e2dee979af194acad7cba96b56bc2f8e644aab97
Luu Nguyen <nxuluu@gmail.com>
2020-08-28 20:26:30 +0700

    Methods of primitive

---
+++ Data types/1. Methods of primitive.js
@@ -0,0 +1,6 @@
+﻿/* 
+    JS cũng cho phép chúng ta làm việc với các primitive như thể chúng là các obj
+    - Cũng có những methods để gọi
+    - Một primitive vẫn là primitive, a giá trị đơn lẻ, Ngôn ngữ vẫn sẽ cho phép truy cập các methods cũng như các thuộc tính của primitive. Để làm được điều đó a obj wrapper được cung cấp để có thể chạy hàm và bị xoá bỏ ngay sau đó
+    - null và undefined thì không có methods
+*/
