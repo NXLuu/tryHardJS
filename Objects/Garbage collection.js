a2cc3d910c6d6233f16b5a58c3e4fc400e64fa58
Luu Nguyen <nxuluu@gmail.com>
2020-08-28 15:45:36 +0700

    References in JS

---
+++ Objects/Garbage collection.js
@@ -0,0 +1,7 @@
+﻿/* 
+    Cách JS phát hiện và xoá những thứ không cần thiết
+    Concept quán lý bộ nhớ chính của JS là Reachability 
+    - Các giá trị có thể truy cập và có khả năng dùng được thì sẽ được đảm bảo lưu trữ trong bộ nhớ
+*/
+
+// Cần đọc thêm 
---
+++ Objects/Object coppying.js
@@ -0,0 +1,34 @@
+﻿/* 
+    Sự khác nhau giữa primitive và objects là object được lưu trữ và copy theo tham chiếu.
+*/
+let user = { name: 'John' };
+
+let admin = user;
+
+admin.name = 'Pete'; // changed by the "admin" reference
+
+alert(user.name); // 'Pete', changes are seen from the "user" reference
+
+// So sánh giữa 2 obj - chúng sẽ bằng nhau néu chúng co cùng một tham chiếu
+
+/* 
+    Cloning and merging, Object.assign
+    - Copy bằng cách tạo mới một object
+    Object.assign(dest, [src1, src2, src3...])
+*/
+let user = { name: "John" };
+
+let permissions1 = { canView: true };
+let permissions2 = { canEdit: true };
+
+// copies all properties from permissions1 and permissions2 into user
+Object.assign(user, permissions1, permissions2);
+
+// now user = { name: "John", canView: true, canEdit: true }
+let user = { name: "John" };
+
+Object.assign(user, { name: "Pete" });
+
+alert(user.name); // now user = { name: "Pete" }
+
+
