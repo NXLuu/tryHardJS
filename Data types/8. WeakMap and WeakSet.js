13261b05319f972b691d3b499f4ce4f84b1818aa
Luu Nguyen <nxuluu@gmail.com>
2020-08-29 20:19:27 +0700

    Add WeakMap and WeakSet

---
+++ Data types/8. WeakMap and WeakSet.js
@@ -0,0 +1,18 @@
+﻿/* 
+    Giá trị key của WeakMap phải là objects
+*/
+
+let weakMap = new WeakMap();
+let obj = {};
+weakMap.set(obj, "ok"); // works fine (object key)
+// can't use a string as the key
+weakMap.set("test", "Whoops"); // Error, because "test" is not an object
+
+let john = { name: "John" };
+
+let weakMap = new WeakMap();
+weakMap.set(john, "...");
+
+john = null; // overwrite the reference
+
+// john is removed from memory!
