1455a94e5e1b160eb4f9e1c507a7bb9525e81614
Luu Nguyen <nxuluu@gmail.com>
2020-08-29 20:10:24 +0700

    Add Map and Set

---
+++ Data types/7. Map and Set.js
@@ -0,0 +1,25 @@
+﻿/* 
+    Map:
+    new Map() - tạo một map
+    map.set(key, value) - lưu giá trị của key
+    map.get(key) - trả về giá trị của key
+    map.has(key) - trả về true nếu key tồn tại, false nếu ngược lại
+    map.delete(key) - xoá giá trị của key
+    map.clear() - xoá mọi thứ của map
+    map.size - trả về số lượng hiện tại phần tử
+
+    map.keys() - trả về một iterable tất cả key
+    map.values() - trả về một iterable tất vả values
+    map.entries() - trả về một iterable cho [key, value]
+*/
+
+/* 
+    Set: Mỗi giá trị chỉ xuất hiện một lần
+    new Set(iterable) - tạo một set, nếu Iterable được cung cấp, 
+    set.add(value) - Thêm một giá trị
+    set.delete(value) - Xoá value khỏi set
+    set.has(value)
+    set.clear()
+    set.size
+    
+*/
