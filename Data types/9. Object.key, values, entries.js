276c9715c76bcc36cbe73992d67e72f1ce1b325a
Luu Nguyen <nxuluu@gmail.com>
2020-08-29 20:31:27 +0700

    How to get keys, values, entries

---
+++ Data types/9. Object.key, values, entries.js
@@ -0,0 +1,8 @@
+﻿/*
+    Object.keys(obj) - trả về mảng của keys
+    Object.values(obj) - trả về mảng các values
+    Object.entries(obj) - trả về một mảng [key, value]
+
+
+    Object thiếu nhiều methods chúng ta có thể sử Object.entries và Object.fromEntries
+*/
