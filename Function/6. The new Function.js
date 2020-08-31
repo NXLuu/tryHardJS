87722cd1440d70093d03eed44a22bfc0b778cdd2
Luu Nguyen <nxuluu@gmail.com>
2020-08-31 15:58:43 +0700

    Add new in Function

---
+++ Function/6. The new Function.js
@@ -0,0 +1,8 @@
+﻿let func = new Function ([arg1, arg2, ...argN], functionBody);
+let sum = new Function('a', 'b', 'return a + b');
+
+alert( sum(1, 2) ); // 3
+
+new Function('a', 'b', 'return a + b'); // basic syntax
+new Function('a,b', 'return a + b'); // comma-separated
+new Function('a , b', 'return a + b'); // comma-separated with spaces
