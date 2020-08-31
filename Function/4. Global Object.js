514ad95954d57b3421994d3c6443b00a6007418d
Luu Nguyen <nxuluu@gmail.com>
2020-08-31 10:20:49 +0700

    Add Global Object

---
+++ Function/4. Global Object.js
@@ -0,0 +1,30 @@
+﻿/* 
+    Global object cung cấp các biến, các hàm ở bất kì đâu. Cái mà là những built trong ngôn ngữ hoặc môi trường
+    - Trong browser là window
+    - Trong Node.js là global
+    - Gần đây globalThis được thêm vào ngôn ngữ
+*/
+alert("Hello");
+// is the same as
+window.alert("Hello");
+var gVar = 5;
+
+alert(window.gVar); // 5 (became a property of the global object)
+
+let gLet = 5;
+
+alert(window.gLet); // undefined (doesn't become a property of the global object)
+
+window.currentUser = {
+  name: "John"
+};
+
+/* 
+    Nó dùng đẻ làm gì:
+    Để kiểm thử các tính năng của ngôn ngữ có được hỗ trọ hay không
+
+*/
+
+if (!window.Promise) {
+  alert("Your browser is really old!");
+}
