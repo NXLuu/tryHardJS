f4c6c2e5b588a4e0d334ecc9636ed0349b7906b4
Luu Nguyen <nxuluu@gmail.com>
2020-09-01 15:35:35 +0700

    instanceof

---
+++ Class/6. Class checking.js
@@ -0,0 +1,16 @@
+﻿/* 
+    instanceof: kiểm tra xem một obj có thuộc 1 class hay kế thừ từ class hay không - nó hoạt động cả với function
+
+*/
+class Rabbit {}
+let rabbit = new Rabbit();
+// is it an object of Rabbit class?
+alert( rabbit instanceof Rabbit ); // true
+
+let arr = [1, 2, 3];
+alert( arr instanceof Array ); // true
+alert( arr instanceof Object ); // true
+
+// instead of class
+function Rabbit() {}
+alert( new Rabbit() instanceof Rabbit ); // true
