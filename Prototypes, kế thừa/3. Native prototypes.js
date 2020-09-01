1d70b49c7feeaa9f78b45fb4a4c543810c406e8a
Luu Nguyen <nxuluu@gmail.com>
2020-09-01 08:23:26 +0700

    native prototypes

---
+++ Prototypes, kế thừa/3. Native prototypes.js
@@ -0,0 +1,13 @@
+﻿/* 
+    Tất cả những built-int constructor function đều có thuộc tính prototype của riêng nó
+*/
+
+/* 
+    Object.prototype
+    obj = {} cũng giống như obj = new Object()
+    Object() cũng là một constructor function
+    -> do đó Object cũng có thuộc tính prototype của riêng nó:  Object.prototype.toString();
+    -> obj.__proto__ = Object.prototype
+    Cũng có những built-in khác như Array, Function, Number
+    - Có thể thêm các property cho prototype cua obj, arr, func khi đó có thể chạy với mội obj, arr, func 
+*/
