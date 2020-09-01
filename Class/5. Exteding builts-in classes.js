db02d7763101b0ec7ff07b06dc29151890c312d6
Luu Nguyen <nxuluu@gmail.com>
2020-09-01 15:21:42 +0700

    Extending builts-in classes

---
+++ Class/5. Exteding builts-in classes.js
@@ -0,0 +1,37 @@
+﻿/* 
+    Thừa kế từ những built-in classes
+    Thừa kế với built-in thì ko thừa kế được static
+*/
+// add one more method to it (can do more)
+class PowerArray extends Array {
+  isEmpty() {
+    return this.length === 0;
+  }
+}
+
+let arr = new PowerArray(1, 2, 5, 10, 50);
+alert(arr.isEmpty()); // false
+
+let filteredArr = arr.filter(item => item >= 10);
+alert(filteredArr); // 10, 50
+
+
+class PowerArray extends Array {
+  isEmpty() {
+    return this.length === 0;
+  }
+
+  // built-in methods will use this as the constructor
+  static get [Symbol.species]() {
+    return Array;
+  }
+}
+
+let arr = new PowerArray(1, 2, 5, 10, 50);
+alert(arr.isEmpty()); // false
+
+// filter creates new array using arr.constructor[Symbol.species] as constructor
+let filteredArr = arr.filter(item => item >= 10);
+
+// filteredArr is not PowerArray, but Array
+alert(filteredArr.isEmpty()); // Error: filteredArr.isEmpty is not a function,./
