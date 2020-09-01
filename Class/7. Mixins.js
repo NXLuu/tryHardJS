9d4fd7b5a3ec1247b820cc77315884d400ddf43b
Luu Nguyen <nxuluu@gmail.com>
2020-09-01 15:39:08 +0700

    Mixins

---
+++ Class/7. Mixins.js
@@ -0,0 +1,28 @@
+﻿/* 
+    Mixin: một class có thể chứa nhiều methods của các class khác
+    - Bằng cách copy methods vào trong prototype
+
+*/
+
+// mixin
+let sayHiMixin = {
+  sayHi() {
+    alert(`Hello ${this.name}`);
+  },
+  sayBye() {
+    alert(`Bye ${this.name}`);
+  }
+};
+
+// usage:
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+// copy the methods
+Object.assign(User.prototype, sayHiMixin);
+
+// now User can say hi
+new User("Dude").sayHi(); // Hello Dude!
