ad293ceb4dabc84a6cfa0ee9649fe3b0606b7aca
Luu Nguyen <nxuluu@gmail.com>
2020-09-01 14:45:40 +0700

    Class inheritance

---
+++ Class/2. Inheritance.js
@@ -0,0 +1,84 @@
+﻿/* 
+    Class inheritance: là một cách class kế thừa từ những class khác
+
+*/
+class Animal {
+  constructor(name) {
+    this.speed = 0;
+    this.name = name;
+  }
+  run(speed) {
+    this.speed = speed;
+    alert(`${this.name} runs with speed ${this.speed}.`);
+  }
+  stop() {
+    this.speed = 0;
+    alert(`${this.name} stands still.`);
+  }
+}
+let animal = new Animal("My animal");
+
+// Kês thừa
+class Rabbit extends Animal {
+  hide() {
+    alert(`${this.name} hides!`);
+  }
+}
+
+let rabbit = new Rabbit("White Rabbit");
+
+rabbit.run(5); // White Rabbit runs with speed 5.
+rabbit.hide(); // White Rabbit hides!
+
+/* 
+    Ghi đè lên một method
+    super.method() - gọi method của bó;
+    super(...) gọi đến parent constructor(chỉ bên trong constructor)
+*/
+class Rabbit extends Animal {
+  stop() {
+    // ...now this will be used for rabbit.stop()
+    // instead of stop() from class Animal
+  }
+}
+
+class Animal {
+  constructor(name) {
+    this.speed = 0;
+    this.name = name;
+  }
+  run(speed) {
+    this.speed = speed;
+    alert(`${this.name} runs with speed ${this.speed}.`);
+  }
+  stop() {
+    this.speed = 0;
+    alert(`${this.name} stands still.`);
+  }
+
+}
+class Rabbit extends Animal {
+  hide() {
+    alert(`${this.name} hides!`);
+  }
+  stop() {
+    super.stop(); // call parent stop
+    this.hide(); // and then hide
+  }
+}
+let rabbit = new Rabbit("White Rabbit");
+rabbit.run(5); // White Rabbit runs with speed 5.
+rabbit.stop(); // White Rabbit stands still. White rabbit hides!
+
+
+/* 
+    Ghi đè lên constructor:
+    - Constructors trong inherititing classes phải gọi super(...) trước khi sử dụng this
+
+*/
+class Rabbit extends Animal {
+  // generated for extending classes without own constructors
+  constructor(...args) {
+    super(...args);
+  }
+}
