d4a9523d492e1064c6c19b03acc4315558d2f98b
Luu Nguyen <nxuluu@gmail.com>
2020-09-02 10:03:52 +0700

    Promise Chaining

---
+++ Promise, async, await/3. Promise chaining.js
@@ -0,0 +1,25 @@
+﻿/* 
+    .then: gọi return ở trong then nó sẽ  trả về kết quả cho promise ban đầu và dùng để gọi các  then tiếp theo
+    .then(handler) - có thể sử dụng để tạo và trả về một promise
+
+*/
+new Promise(function(resolve, reject) {
+
+  setTimeout(() => resolve(1), 1000); // (*)
+
+}).then(function(result) { // (**)
+
+  alert(result); // 1
+  return result * 2;
+
+}).then(function(result) { // (***)
+
+  alert(result); // 2
+  return result * 2;
+
+}).then(function(result) {
+
+  alert(result); // 4
+  return result * 2;
+
+});
