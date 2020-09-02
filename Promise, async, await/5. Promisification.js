4b21be4519736d02b389aecd6a8b1ccb2187c734
Luu Nguyen <nxuluu@gmail.com>
2020-09-02 15:42:59 +0700

    Promisification

---
+++ Promise, async, await/5. Promisification.js
@@ -0,0 +1,49 @@
+﻿/* 
+    Promisification - một sự biến đổi đơn giản. Nó kà sự chuyển một hàm với callback thành một hàm trả về promise
+*/
+function loadScript(src, callback) {
+  let script = document.createElement('script');
+  script.src = src;
+
+  script.onload = () => callback(null, script);
+  script.onerror = () => callback(new Error(`Script load error for ${src}`));
+
+  document.head.append(script);
+}
+// usage:
+// loadScript('path/script.js', (err, script) => {...})
+
+
+let loadScriptPromise = function(src) {
+  return new Promise((resolve, reject) => {
+    loadScript(src, (err, script) => {
+      if (err) reject(err)
+      else resolve(script);
+    });
+  })
+}
+
+// usage:
+// loadScriptPromise('path/script.js').then(...)
+
+function promisify(f) {
+  return function (...args) { // return a wrapper-function
+    return new Promise((resolve, reject) => {
+      function callback(err, result) { // our custom callback for f
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+
+      args.push(callback); // append our custom callback to the end of f arguments
+
+      f.call(this, ...args); // call the original function
+    });
+  };
+};
+
+// usage:
+let loadScriptPromise = promisify(loadScript);
+loadScriptPromise(...).then(...);
