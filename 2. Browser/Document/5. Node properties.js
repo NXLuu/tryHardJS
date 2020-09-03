2b2570c4ca4849675862b5493238021aabe7e298
Luu Nguyen <nxuluu@gmail.com>
2020-09-03 10:05:04 +0700

    Atributes

---
+++ 2. Browser/Document/5. Node properties.js
@@ -0,0 +1,31 @@
+﻿/* 
+                  EventTarget
+                       |
+                       |
+                     Node
+                       |
+        Text        Element   Comment
+                       |
+                    HTMLElement
+HTMLInputElement  HTMLBodyElement   HTMLAnch
+
+    - EventTarget - Là một class gốc trừu tượng. Obj của class này thì không bao giờ được tạo - là cơ sở của DOM nodes
+    - Node  - Là cơ sở của DOM
+    - Elements - Là cơ sở của DOM elements
+*/
+
+
+/* 
+    - elem.nodeType - loại node của elem
+        1 - là elements node
+        3 - là text node
+        9 - là document obj
+    - nodeName, tagName - 
+        1. tagName chỉ tồn tại cho elemets node
+        2. nodename tồn tại cho bất kì node nào
+    - elem.innerHTML - lấy HTML bên trongg elements -> trả về string
+    - elem.outer - Nó giống như innerHTML nhưng bao gồm HTML của chính elem
+    - elem.nodeValue/data - trả về text node
+    - elem.textContent - trả về text bên trong element kể  cả xuống dòng
+    - elem.hidden - ẩn elem
+*/
---
+++ 2. Browser/Document/6. Atrributes.js
@@ -0,0 +1,20 @@
+﻿/* 
+    Properties:
+        1. Loại giá trị là bất kì giá trị nào
+        2. Tên phân biệt chữ hoa chữ thường
+    Attributes:
+        1. loại là một string
+        2. Tên không phân biệt chữa hoa
+    - elem.hasAttribute(name) - Kiểm tra xem một attributes có tồn tại
+
+    - elem.getAttribute(name) – trả về giá trị của Attributes.
+    - elem.setAttribute(name, value) – set giá trị.
+    - elem.removeAttribute(name) – to remove the attribute.
+    - elem.attributes is a collection of all attributes.
+
+*/
+
+/* 
+    data-nameAtrributes (id)
+    let atrributes = id.dataset.nameAtrributes
+*/
