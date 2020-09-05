25d5a17f900e6cce937d9d918d52cf3226989ef9
Luu Nguyen <nxuluu@gmail.com>
2020-09-05 07:33:20 +0700

    Element size

---
+++ 2. Browser/Document/9. Element size and scrolling.js
@@ -0,0 +1,33 @@
+﻿/* 
+    - offsetParent- Tổ tiên gần nhất của elem dùng để tính toàn
+        1. Nó có position: absolute, relative, fixed, ssticky
+        2. <td>, <th>, table
+        3. <body>
+    - offsetLeft- Bên trái so với offserParent
+
+    - offsetTop- Bên trên so với parent
+*/
+
+/* 
+    Thuộc tính của phần tử:
+    - offsetWidth - Chiều rộng của elem tính cả padding và border
+    - offsetHeight- Chiều cao của elem tính cả
+
+    - clientTop - Chiều rộng border bên  trên
+
+    -clientLeft - Chiều rộng border bên trái
+
+    - clientWidth - Chiều rộng content cùng với padding
+
+   - clientHeight - Chiều cao content cùng padding
+
+   - scrollHeight - Chiêu cao của toàn bộ content kể cả phần bị ẩn
+   
+   - scrollWidth - Chiều rộng 
+
+   - scrollLeft - 
+
+    - scrollTop - Chiều cao của phần bị ẩn đi 
+
+    - scrollLeft - chiều rộng của phần bị ẩn đi
+*/
