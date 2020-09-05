a1990392ca4a914bcf723e0056d95c85e9ad502e
Luu Nguyen <nxuluu@gmail.com>
2020-09-05 15:38:58 +0700

    Window size and scrolling

---
+++ 2. Browser/Document/10. Window sizes and scrolling.js
@@ -0,0 +1,28 @@
+﻿/*
+    document.documentElement - HTML
+    + Để lấy chiều cao, chiều rộng của window
+        - document.documentElement.clientWidth 
+        - document.documentElement.clientHeight
+
+    Lấy chiều cao của toàn bộ document kẻ cả scrolled
+    let scrollHeight = Math.max(
+    document.body.scrollHeight, document.documentElement.scrollHeight,
+    document.body.offsetHeight, document.documentElement.offsetHeight,
+    document.body.clientHeight, document.documentElement.clientHeight
+    );
+
+    document.documentElement.scrollLeft/Top
+    window.pageYOffset/pageXOffset
+
+    + Scrolling:
+        - window.scrollTo(pageX,pageY): - scroll từ vị trí hiện tại pageX và pageY
+        - window.scrollBy(x,y) - Cuộn trang tới chính xác x, y
+        - elem.scrollIntoView(top): Cuộn trang cho tới khi elem đcợc nhìn thấy
+            1. top=true: elem ở bên  trên
+            2. top=fale: elem sẽ ở bên dưới
+
+    + Cấm không cho cuộn:
+        - document.body.style.overflow = "hidden"
+        - document.body.style.overflow = "":
+        tiếp tục cuộn
+*/
